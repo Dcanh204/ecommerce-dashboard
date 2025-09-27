f4d41ad82f5253847ec406daa20e1aa046238a46
Dinh Canh <dinhcanhh2004@gmail.com>
2025-09-27 22:09:10 +0700

    feat: add login page UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 const App = lazy(() => import('./App.jsx'))
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Suspense>
+      <App />
+    </Suspense>
   </BrowserRouter>
 )
