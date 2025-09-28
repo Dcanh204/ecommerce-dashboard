f941fcbccb27bc9c2b92bf59232b062232deee86
Dinh Canh <dinhcanhh2004@gmail.com>
2025-09-28 22:18:45 +0700

    feat: add admin login page UI

---
+++ src/pages/auth/AdminLogin.jsx
@@ -0,0 +1,50 @@
+
+import { useState } from 'react';
+const AdminLogin = () => {
+
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  })
+  const inputHandle = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value
+    })
+  }
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    console.log(state);
+  }
+  return (
+    <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
+      <div className="w-[350px] text-[#ffffff] p-2">
+        <div className='bg-[#6f68d1] p-4 rounded-md'>
+          <div className='h-[70px] flex justify-center items-center'>
+            <div className='w-[180px] h-[50px]'>
+              <img className='w-full h-full' src="/images/logo.png" alt="logo" />
+            </div>
+          </div>
+          <form onSubmit={submitHandle}>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Email</label>
+              <input onChange={inputHandle} value={state.email} className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md" type="email" name="email" id="email" placeholder="Email" required />
+            </div>
+
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="password">Password</label>
+              <input onChange={inputHandle} value={state.password} className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md" type="password" name="password" id="password" placeholder="Password" required />
+            </div>
+
+            <button className="bg-slate-800 w-full hover:shadow-blue-300 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3 cursor-pointer">Sign In</button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default AdminLogin;
