c95696cbc6521590f6f1a3ae151c62bcfc5c767e
Dinh Canh <dinhcanhh2004@gmail.com>
2025-11-04 22:56:46 +0700

    feat: update auth reducer

--- src/stores/Reducers/authReducer.js
+++ src/stores/Reducers/authReducer.js
@@ -1,17 +1,73 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { api } from './../../api/api';
-
+import { jwtDecode } from 'jwt-decode';
 export const admin_login = createAsyncThunk(
   'auth/admin_login',
   async (info, { rejectWithValue }) => {
     try {
-      const { data } = await api.post('/admin-login', info, { withCredentials: true })
+      const { data } = await api.post('auth/admin-login', info, { withCredentials: true })
+      localStorage.setItem('accessToken', data.token);
       return data;
     } catch (error) {
-      return rejectWithValue(error.response.data.message);
+      return rejectWithValue(error.response.data);
     }
   }
 )
+
+export const seller_login = createAsyncThunk(
+  'auth/seller_login',
+  async (info, { rejectWithValue }) => {
+    try {
+      const { data } = await api.post('auth/seller-login', info, { withCredentials: true })
+      localStorage.setItem('accessToken', data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+)
+
+export const seller_register = createAsyncThunk(
+  'auth/seller_register',
+  async (info, { rejectWithValue }) => {
+    try {
+      const { data } = await api.post('auth/seller-register', info, { withCredentials: true })
+      localStorage.setItem('accessToken', data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
+  }
+)
+
+export const get_me = createAsyncThunk(
+  'auth/get_me',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await api.get('auth/get-me', { withCredentials: true })
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+)
+
+const returnRole = (token) => {
+  try {
+    if (!token) return '';
+    const decodedToken = jwtDecode(token);
+    const expireTime = new Date(decodedToken.exp * 1000);
+    if (new Date() > expireTime) {
+      localStorage.removeItem('accessToken');
+      return null;
+    }
+    return decodedToken.role || '';
+  } catch {
+    localStorage.removeItem('accessToken');
+    return null;
+  }
+
+}
 
 const authReducer = createSlice({
   name: 'auth',
@@ -19,11 +75,14 @@ const authReducer = createSlice({
     successMessage: '',
     errorMessage: '',
     loading: false,
-    userInfo: ''
+    userInfo: '',
+    role: returnRole(localStorage.getItem('accessToken')),
+    token: localStorage.getItem('accessToken')
   },
   reducers: {
     messageClear: (state) => {
-      state.errorMessage = ''
+      state.errorMessage = '';
+      state.successMessage = ''
     }
   },
 
@@ -34,12 +93,55 @@ const authReducer = createSlice({
       })
       .addCase(admin_login.fulfilled, (state, action) => {
         state.loading = false;
-        state.successMessage = action.payload.message
+        state.successMessage = action.payload.message;
+        state.token = action.payload.token;
+        state.role = returnRole(action.payload.token);
       })
       .addCase(admin_login.rejected, (state, action) => {
         state.loading = false;
-        state.errorMessage = action.payload;
+        state.errorMessage = action.payload.message;
       })
+
+      .addCase(seller_register.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(seller_register.fulfilled, (state, action) => {
+        state.loading = false;
+        state.successMessage = action.payload.message;
+        state.token = action.payload.token;
+        state.role = returnRole(action.payload.token);
+      })
+      .addCase(seller_register.rejected, (state, action) => {
+        state.loading = false;
+        state.errorMessage = action.payload.message;
+      })
+
+      .addCase(seller_login.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(seller_login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.successMessage = action.payload.message;
+        state.token = action.payload.token;
+        state.role = returnRole(action.payload.token);
+      })
+      .addCase(seller_login.rejected, (state, action) => {
+        state.loading = false;
+        state.errorMessage = action.payload.message;
+      })
+
+      .addCase(get_me.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(get_me.fulfilled, (state, action) => {
+        state.loading = false;
+        state.userInfo = action.payload.userInfo;
+      })
+      .addCase(get_me.rejected, (state, action) => {
+        state.loading = false;
+        state.errorMessage = action.payload.message;
+      })
+
   }
 })
 
