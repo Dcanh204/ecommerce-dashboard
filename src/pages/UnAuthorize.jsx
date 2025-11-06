2534be139d970064ad6784b4ba5ee5893bef8e4f
Dinh Canh <dinhcanhh2004@gmail.com>
2025-11-06 23:31:24 +0700

    feat: add UnAuthorized  page UI

---
+++ src/pages/UnAuthorize.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UnAuthorize = () => {
+  return (
+    <div className='h-screen flex justify-center items-center bg-[#f2f9ff]'>
+      <img className='w-[600px] h-[500px]' src="/images/unauthorized.jpg" alt="unauthorized" />
+    </div>
+  );
+};
+
+export default UnAuthorize;
