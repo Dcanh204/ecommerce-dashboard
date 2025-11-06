9fb5e4bdae07617fdeca7dd479d549984fd1dbb0
Dinh Canh <dinhcanhh2004@gmail.com>
2025-11-06 23:30:44 +0700

    feat: add not found page UI

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='px-2 lg:py-7 py-5'>
+      <div className='w-full flex justify-center items-center'>
+        <img className='w-[600px] h-[600px] ' src="/images/404.png" alt="404" />
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
